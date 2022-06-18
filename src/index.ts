0efbc64d59207fcfbc8fff5d9dbcefaa17f66110
anna-left <anna_left@mail.ru>
2022-06-18 20:24:03 +0300

    feat: start with multi argument

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,27 @@ import "dotenv/config";
 import { server } from "./server";
 
 const PORT = Number(process.env.PORT) || 3000;
+let multiMode = process.argv[2];
 
-if (cluster.isPrimary === true) {
-  const CPUS: any = cpus();
-  console.log(`The number of host machine logical CPU cores: ${CPUS.length}`);
-  console.log("--------------------------------------------------");
+if (multiMode) {
+  if (cluster.isPrimary === true) {
+    const CPUS: any = cpus();
+    console.log(`The number of host machine logical CPU cores: ${CPUS.length}`);
+    console.log("--------------------------------------------------");
 
-  CPUS.forEach(() => cluster.fork());
-  cluster.on("exit", () => {
-    cluster.fork();
-  });
+    CPUS.forEach(() => cluster.fork());
+    cluster.on("exit", () => {
+      cluster.fork();
+    });
+  } else {
+    server.listen(PORT, "127.0.0.1", () => {
+      console.log(
+        `API is listening on http://localhost:${PORT} on ${process.pid}`
+      );
+    });
+  }
 } else {
   server.listen(PORT, "127.0.0.1", () => {
-    console.log(
-      `API is listening on http://localhost:${PORT} on ${process.pid}`
-    );
+    console.log(`Сервер начал прослушивание запросов на ${PORT}`);
   });
 }
