234a6f5d0da984c3189a4dee98af51a212ff89e0
anna-left <anna_left@mail.ru>
2022-06-18 16:47:13 +0300

    feat: add horizontal scaling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
+import cluster from "cluster";
+import { cpus } from "os";
 import "dotenv/config";
 
 import { server } from "./server";
 
 const PORT = Number(process.env.PORT) || 3000;
 
-server.listen(PORT, "127.0.0.1", () => {
-  console.log(`Сервер начал прослушивание запросов на ${PORT}`);
-});
+if (cluster.isPrimary === true) {
+  const CPUS: any = cpus();
+  console.log(`The number of host machine logical CPU cores: ${CPUS.length}`);
+  console.log("--------------------------------------------------");
+
+  CPUS.forEach(() => cluster.fork());
+  cluster.on("exit", () => {
+    cluster.fork();
+  });
+} else {
+  server.listen(PORT, "127.0.0.1", () => {
+    console.log(
+      `API is listening on http://localhost:${PORT} on ${process.pid}`
+    );
+  });
+}
